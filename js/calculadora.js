ddc4af01dde3f4494f67d6d6fbefe204bfa34b6f
Ana Beatriz de Sousa Costa <ana.costa@easynvest.com.br>
2019-08-12 14:54:18 -0300

    Adicionando primeira versão da calculadora, apenas 4 operações

---
+++ js/calculadora.js
@@ -0,0 +1,82 @@
+var numClick = document.querySelector('.nums').addEventListener('click', guardarNumeros)
+
+let v1
+let v2
+let operador
+
+
+
+function guardarNumeros() {
+    let clicado = event.target.innerText
+    resultado.innerHTML += clicado
+}
+
+
+let operacao = document.querySelector('.btn-opr').addEventListener('click', guardarOperador)
+function guardarOperador(event) {
+console.log('oi')
+    let display = document.querySelector('#resultado').innerText
+    v1 = display
+
+
+    let clicado = event.target.innerText
+    operador = clicado
+
+    resultado.innerHTML = ''
+}
+
+
+
+function guardar2() {
+    v2 = resultado.innerHTML
+}
+
+
+
+let igual = document.querySelector('.igual').addEventListener('click', fazerConta)
+
+function fazerConta() {
+
+    guardar2()
+
+
+    if (operador == '+') {
+        conta = (Number(v1) + Number(v2))
+        resultado.innerHTML = conta
+    }
+
+    else if (operador == '-') {
+        conta = (Number(v1) - Number(v2))
+        resultado.innerHTML = conta
+    }
+
+    else if (operador == 'x') {
+        conta = (Number(v1) * Number(v2))
+        resultado.innerHTML = conta
+    }
+
+    else {
+        conta = (Number(v1) / Number(v2))
+        resultado.innerHTML = conta.toFixed(1)
+
+        if (conta == 'Infinity'){
+            resultado.innerHTML = 0
+        }
+    }
+}
+
+
+
+document.querySelector('.eraser').addEventListener('click', apagar)
+function apagar(){
+
+    let eraser = event.target.innerText
+
+    if(eraser == 'C'){
+        resultado.innerText = ''
+    }
+
+    else{
+        resultado.innerText = resultado.innerText.slice(0, -1)
+    }
+}
