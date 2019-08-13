2b328b00206fc498ebec7aa4b1f7a845ae2a1210
Ana Beatriz de Sousa Costa <ana.costa@easynvest.com.br>
2019-08-13 15:23:19 -0300

    Teclas dos numeros funcionando

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,13 +1,63 @@
-var numClick = document.querySelector('.nums').addEventListener('click', guardarNumeros)
+let numClick = document.querySelector('.nums').addEventListener('click', guardarNumeros)
 let operacao = document.querySelector('.btn-opr').addEventListener('click', guardarOperador)
 let igual = document.querySelector('.igual').addEventListener('click', fazerConta)
 document.querySelector('.eraser').addEventListener('click', apagar)
 document.querySelector('.ponto').addEventListener('click', ponto)
 
+
+document.addEventListener('keydown', tecla)
+
+
+function tecla() {
+    resultado.innerText += event.key1234
+
+
+    // if (tecla_pressionada == 48) {
+    //     resultado.innerHTML += 0
+    // } else if (tecla_pressionada == 49) {
+    //     resultado.innerHTML += 1
+    // } else if (tecla_pressionada == 50) {
+    //     resultado.innerHTML += 2
+    // } else if (tecla_pressionada == 51) {
+    //     resultado.innerHTML += 3
+    // } else if (tecla_pressionada == 52) {
+    //     resultado.innerHTML += 4
+    // } else if (tecla_pressionada == 53) {
+    //     resultado.innerHTML += 5
+    // } else if (tecla_pressionada == 54) {
+    //     resultado.innerHTML += 6
+    // } else if (tecla_pressionada == 55) {
+    //     resultado.innerHTML += 7
+    // } else if (tecla_pressionada == 56) {
+    //     resultado.innerHTML += 8
+    // } else if (tecla_pressionada == 57) {
+    //     resultado.innerHTML += 9
+    // }else if (tecla_pressionada == 27) {
+    //     resultado.innerHTML = ' '
+    // }
+    //  else if(tecla_pressionada == 27 || tecla_pressionada == 46){
+    //     resultado.innerText = ''
+    // }
+
+    // else if(tecla_pressionada == 8){
+    //     resultado.innerText = resultado.innerText.slice(0, -1)
+    // }
+
+    // else{
+    //     operadorTeclado = tecla_pressionada
+    //     v1 = resultado.innerText
+    //     resultado.innerText = ''
+    // }
+}
+
+
+
+
 let contar_pontos = 0
 let v1
 let v2
 let operador
+let operadorTeclado
 
 
 
@@ -24,6 +74,8 @@ function guardarNumeros() {
 // GUARDA O OPERADOR
 function guardarOperador(event) {
 
+    operadorTeclado
+
     contar_pontos = 0
     let display = document.querySelector('#resultado').innerText
     v1 = display
@@ -33,6 +85,8 @@ function guardarOperador(event) {
     operador = clicado
 
     resultado.innerHTML = ''
+
+    console.log(`Olha eu aqui ${operadorTeclado}`)
 }
 
 
@@ -45,24 +99,32 @@ function guardar2() {
 
 
 // FAZ A CONTA
-function fazerConta() {
+function fazerConta() {12
     contar_pontos = 0
     guardar2()
 
-    if (operador == '+') {
+    console.log(`TOTAL: ${v1}, ${operadorTeclado}, ${v2}`)
+
+    if (operador == '+' || operadorTeclado == 43) {
         conta = (Number(v1) + Number(v2))
         resultado.innerHTML = conta
-    }else if (operador == '-') {
+    } 
+    
+    else if (operador == '-' || operadorTeclado == 45) {
         conta = (Number(v1) - Number(v2))
         resultado.innerHTML = conta
-    }else if (operador == 'x') {
+    } 
+    
+    else if (operador == 'x' || operadorTeclado == 42) {
         conta = (Number(v1) * Number(v2))
         resultado.innerHTML = conta
-    }else {
+    } 
+    
+    else {
         conta = (Number(v1) / Number(v2))
         resultado.innerHTML = conta.toFixed(1)
 
-        if (conta == 'Infinity'){
+        if (conta == 'Infinity') {
             resultado.innerHTML = 'Erro'
         }
     }
@@ -70,15 +132,16 @@ function fazerConta() {
 
 
 
+
 // APAGA OS RESULTADOS
-function apagar(){
+function apagar() {
 
     let eraser = event.target.innerText
 
-    if(eraser == 'C'){
+    if (eraser == 'C') {
         resultado.innerText = ''
         contar_pontos = 0
-    }else{
+    } else {
         resultado.innerText = resultado.innerText.slice(0, -1)
     }
 }
@@ -87,16 +150,17 @@ function apagar(){
 
 
 // FUNÇÃO DO PONTO
-function ponto(){
+function ponto() {
     contar_pontos++
 
 
-    if(event.target.innerText == '.' && resultado.innerText == ''){
+    if (event.target.innerText == '.' && resultado.innerText == '') {
         resultado.innerHTML = '0'
-    } 
+    }
 
-    
-    if(contar_pontos > 1){
+
+    if (contar_pontos > 1) {
         resultado.innerText = 'Erro'
     }
 }
+
