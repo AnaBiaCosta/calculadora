8d9b573853d8ae48113dbe4898ef2466920cdada
Ana Beatriz de Sousa Costa <ana.costa@easynvest.com.br>
2019-08-14 14:49:05 -0300

    Adionando operações pelo teclado e verificações para apagar valores

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,53 +3,7 @@ let operacao = document.querySelector('.btn-opr').addEventListener('click', guar
 let igual = document.querySelector('.igual').addEventListener('click', fazerConta)
 document.querySelector('.eraser').addEventListener('click', apagar)
 document.querySelector('.ponto').addEventListener('click', ponto)
-
-
-document.addEventListener('keydown', tecla)
-
-
-function tecla() {
-    resultado.innerText += event.key1234
-
-
-    // if (tecla_pressionada == 48) {
-    //     resultado.innerHTML += 0
-    // } else if (tecla_pressionada == 49) {
-    //     resultado.innerHTML += 1
-    // } else if (tecla_pressionada == 50) {
-    //     resultado.innerHTML += 2
-    // } else if (tecla_pressionada == 51) {
-    //     resultado.innerHTML += 3
-    // } else if (tecla_pressionada == 52) {
-    //     resultado.innerHTML += 4
-    // } else if (tecla_pressionada == 53) {
-    //     resultado.innerHTML += 5
-    // } else if (tecla_pressionada == 54) {
-    //     resultado.innerHTML += 6
-    // } else if (tecla_pressionada == 55) {
-    //     resultado.innerHTML += 7
-    // } else if (tecla_pressionada == 56) {
-    //     resultado.innerHTML += 8
-    // } else if (tecla_pressionada == 57) {
-    //     resultado.innerHTML += 9
-    // }else if (tecla_pressionada == 27) {
-    //     resultado.innerHTML = ' '
-    // }
-    //  else if(tecla_pressionada == 27 || tecla_pressionada == 46){
-    //     resultado.innerText = ''
-    // }
-
-    // else if(tecla_pressionada == 8){
-    //     resultado.innerText = resultado.innerText.slice(0, -1)
-    // }
-
-    // else{
-    //     operadorTeclado = tecla_pressionada
-    //     v1 = resultado.innerText
-    //     resultado.innerText = ''
-    // }
-}
-
+document.addEventListener('keydown', teclado)
 
 
 
@@ -57,11 +11,54 @@ let contar_pontos = 0
 let v1
 let v2
 let operador
+let apagador = ''
 let operadorTeclado
+let pontoTeclado
 
 
+// PEGAR OS VALORES DO TECLADO
+function teclado() {
+    let tecla = event.key
+    apagador = tecla
+
+    if (tecla == '0' || tecla == '1' || tecla == '2' || tecla == '3' || tecla == '4' || tecla == '5' || tecla == '6'
+        || tecla == '7' || tecla == '8' || tecla == '9') {
+        resultado.innerText += tecla
+    }
+
+    else if (tecla == 'Delete' || tecla == 'Backspace' || tecla == 'Escape') {
+        apagador = tecla
+        apagar()
+    }
+
+    else if (tecla == '=') {
+        fazerConta()
+    }
+
+    else if(tecla == '.'){
+        resultado.innerText += tecla
+        pontoTeclado = tecla
+
+        if (contar_pontos > 1) {
+            event.preventDefault()
+        }
+        ponto()
+    }
+
+    else if(tecla == '+' || tecla == '-' || tecla == '*' || tecla == '/') {
+        operadorTeclado = event.key
+        guardarOperadorTeclado()
+    }
+}
 
 
+// // OPERADOR DO TECLADO
+function guardarOperadorTeclado() {
+    contar_pontos = 0
+    v1 = resultado.innerText
+    operador = operadorTeclado
+    resultado.innerText = ''
+}
 
 // GUARDA O PRIMEIRO NÚMERO
 function guardarNumeros() {
@@ -70,23 +67,13 @@ function guardarNumeros() {
 }
 
 
-
 // GUARDA O OPERADOR
 function guardarOperador(event) {
-
-    operadorTeclado
-
     contar_pontos = 0
-    let display = document.querySelector('#resultado').innerText
-    v1 = display
-
-
+    v1 = resultado.innerText
     let clicado = event.target.innerText
     operador = clicado
-
     resultado.innerHTML = ''
-
-    console.log(`Olha eu aqui ${operadorTeclado}`)
 }
 
 
@@ -97,29 +84,26 @@ function guardar2() {
 }
 
 
-
 // FAZ A CONTA
-function fazerConta() {12
+function fazerConta() {
     contar_pontos = 0
     guardar2()
 
-    console.log(`TOTAL: ${v1}, ${operadorTeclado}, ${v2}`)
-
-    if (operador == '+' || operadorTeclado == 43) {
+    if (operador == '+') {
         conta = (Number(v1) + Number(v2))
         resultado.innerHTML = conta
-    } 
-    
-    else if (operador == '-' || operadorTeclado == 45) {
+    }
+
+    else if (operador == '-') {
         conta = (Number(v1) - Number(v2))
         resultado.innerHTML = conta
-    } 
-    
-    else if (operador == 'x' || operadorTeclado == 42) {
+    }
+
+    else if (operador == 'x' || operador == '*') {
         conta = (Number(v1) * Number(v2))
         resultado.innerHTML = conta
-    } 
-    
+    }
+
     else {
         conta = (Number(v1) / Number(v2))
         resultado.innerHTML = conta.toFixed(1)
@@ -131,14 +115,11 @@ function fazerConta() {12
 }
 
 
-
-
 // APAGA OS RESULTADOS
 function apagar() {
-
     let eraser = event.target.innerText
 
-    if (eraser == 'C') {
+    if (eraser == 'C' || apagador == 'Delete' || apagador == 'Escape') {
         resultado.innerText = ''
         contar_pontos = 0
     } else {
@@ -152,15 +133,16 @@ function apagar() {
 // FUNÇÃO DO PONTO
 function ponto() {
     contar_pontos++
+    console.log('tela ' + resultado.innerText)
 
 
     if (event.target.innerText == '.' && resultado.innerText == '') {
         resultado.innerHTML = '0'
     }
-
-
-    if (contar_pontos > 1) {
-        resultado.innerText = 'Erro'
+    else if(pontoTeclado == '.' && resultado.innerText == '.') {
+        resultado.innerHTML = '0.'
     }
+
+  
 }
 
